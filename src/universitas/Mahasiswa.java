f0259ff1159202cd75ac3ece6bbd73dd5bc22c36
mustafakamal0799 <mustafakamal130799@gmail.com>
2023-06-13 20:49:32 +0800

    Membuat Class Lingkaran dan PersegiPanjang

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universitas;
 
-/**
- *
- * @author MUSTAFA KAMAL
- */
 public class Mahasiswa {
+    String npm, nama, prodi;
     
+    public Mahasiswa(String npm, String nama, String prodi){
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+    
+    public void setNPM (String npm){
+        this.npm = npm;
+    }
+    public void setNama (String nama){
+        this.prodi = nama;
+    }
+    public void setProdi (String prodi){
+        this.prodi = prodi;
+    }
 }
