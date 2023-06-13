6a0cc7cf44ebc943e431ba46bc2d36d9210cf926
mustafakamal0799 <mustafakamal130799@gmail.com>
2023-06-13 20:05:04 +0800

    Membuat class Mahasiswa dan Mahasiswa Beraksi

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package latihan2;
+package universitas;
 
 /**
  *
  * @author MUSTAFA KAMAL
  */
-public class Latihan2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class MahasiswaBeraksi {
     
 }
